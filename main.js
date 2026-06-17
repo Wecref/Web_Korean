ae966ffadd0fc6ec5deff1a8b399a4814ef0f48c
Wecref <wecref.dev@gmail.com>
2026-06-17 10:37:53 +0900

    초기 설정: Web_Wecref 베이스 프로젝트 복사 (신규 git 저장소)

---
+++ main.js
@@ -0,0 +1,7 @@
+import * as nav from './components/nav/Nav.js';
+
+document.addEventListener('DOMContentLoaded', () => {
+    nav.InitMenu(document.querySelectorAll('nav .nav-menus a')); // li 대신 a 태그에 이벤트 리스너를 추가합니다.
+    nav.InitMenuIcon(document.querySelector('.menu-toggle'), document.querySelector('.nav-body'));
+    nav.InitLanguageMenu(document.querySelectorAll('.language-selector a')); // li 대신 a 태그에 이벤트 리스너를 추가합니다.
+});
